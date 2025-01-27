e65827ca4003022b2bce5c77b5eae95f2e582ba2
Sahil yadav <sahil9082811@gmail.com>
2025-01-27 05:14:15 -0600

    next js with redux js

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const page = () => {
+// const callingApi = async (formdata:FormData) => {
+// "use server"
+// const title= formdata.get("name")
+// }
+
+
   return (
-    <div>Login page</div>
+    <>
+    <form>
+      <div>
+        <label htmlFor="">name</label>
+        <input type="text" id='' name="name" />
+      </div>
+      <div>
+        <label htmlFor="">Email</label>
+        <input type="text"  name="email"/>
+      </div>
+      <div>
+        <label htmlFor="">Phone</label>
+        <input type="text" name="phone" />
+      </div>
+    </form>
+    </>
   )
 }
 
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+  return (
+    <div>product page
+      
+      <Button>Button</Button></div>
+  )
+}
+
+export default page
